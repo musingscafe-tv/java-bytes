71cbc2c50d4751b8f97822d0448a0b5c03faf658
MusingsCafe-tv <musingscafe.tv@gmail.com>
2019-01-20 11:15:03 +0530

    Initial Check-in

---
+++ src/test/java/com/musingscafe/AppTest.java
@@ -0,0 +1,20 @@
+package com.musingscafe;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
